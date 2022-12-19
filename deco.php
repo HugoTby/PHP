5959bb137f8a7501529be1bdb5e916286d3a87d0
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2022-12-19 13:55:38 +0100

    Add files via upload

---
+++ deco.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+session_destroy();
+header('location: ../final.php');
+exit;
+?>
+
+<!-- par Hugo Tabary BTSSN1 - Octobre 2022
+Vérifié le 19/12/2022 -->
---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+include("final.php");
+$_SESSION["Login"] = $_POST['login'];
+$_SESSION["Password"] = $_POST['password'];
+echo renvoi($_SESSION["Login"], $_SESSION["Password"]);
+?>
+
+<!-- par Hugo Tabary BTSSN1 - Octobre 2022
+Vérifié le 19/12/2022 -->
