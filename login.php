cc5271ff679ea5b39cb52969a1ae0fae1618972c
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2022-12-19 14:01:53 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("final.php");
+include("index.php");
 $_SESSION["Login"] = $_POST['login'];
 $_SESSION["Password"] = $_POST['password'];
 echo renvoi($_SESSION["Login"], $_SESSION["Password"]);
