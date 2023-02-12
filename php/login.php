cd66baa952ba0f7566b13c8bcb8c13b50f39b7a8
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-02-12 21:01:42 +0100

    Delete php directory

--- php/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include("final.php");
-$_SESSION["Login"] = $_POST['login'];
-$_SESSION["Password"] = $_POST['password'];
-echo renvoi($_SESSION["Login"], $_SESSION["Password"]);
-?>
--- php/reussite2.php
+++
@@ -1,8 +0,0 @@
-<h1> Mot de passe et login sont corrects ! vous pouvez désormais accéder au site </h1>
-
-
-<form action="disconnect2.php" method="post">
-        
-        <input type="submit" id="disconnect" value="Déconnexion">
-        
-</form>
